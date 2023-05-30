31f8de7396fef95f2737a2c8480f486609165244
sreardonyext <100706230+sreardonyext@users.noreply.github.com>
2023-05-30 16:37:10 -0400

    Update sample-dm-function.js

--- sample-dm-function.js
+++ sample-dm-function.js
@@ -6,11 +6,11 @@ export function handleEntityNotification (data: any) {
     const type = data.meta.eventType; // This is from the webhook
     const entityType = data.primaryProfile.meta.entityType; 
     if (type === "ENTITY_UPDATED" && (entityType === "ce_referral")) {
-        var candidateName = data.candidateFirstName + data.candidateLastName; // First and Last name of candidate. Will pass in Slakc messages to referrers. 
-        var referrerEmail = data.meta;// Add streams value for referrer email -- add logic to grab the one that ends in @yext.com
-        var referrerFirstName = data.c_yexterWhoReferredThisPerson.firstName; // Pull first name of referrer to use when addressing in Slack message. 
-        var referralJob = data.referralJob.name; // Pull name of Job linked entity that the candidate was referred for. 
-        var newStatus = data.changedFields.fieldNames.c_referralStatus; // This path should be the field value of c_referralStatus (In Review, Offer, Hired, Rejected). This will be used to determine the body text to send in the message below. 
+        var candidateName = response.entities.name; // First and Last name of candidate. Will pass in Slakc messages to referrers. 
+        var referrerEmail = response.entities.c_referralEmailAddress;// Add streams value for referrer email -- add logic to grab the one that ends in @yext.com
+        var referrerFirstName = response.entities.c_yexterWhoReferredThisPerson.firstName; // Pull first name of referrer to use when addressing in Slack message. 
+        var referralJob = response.entities.c_referralJob.name; // Pull name of Job linked entity that the candidate was referred for. 
+        var newStatus = response.entities.c_referralStatus; // This path should be the field value of c_referralStatus (In Review, Offer, Hired, Rejected). This will be used to determine the body text to send in the message below. 
         return updateMessageHandler(candidateName, referrerEmail, referrerFirstName, referralJob, newStatus);
     }
     return null;
