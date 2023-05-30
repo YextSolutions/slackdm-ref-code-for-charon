5cf355a1f5cd7d8886f990b179531f10aab4732a
sreardonyext <100706230+sreardonyext@users.noreply.github.com>
2023-05-30 15:07:17 -0400

    Add files via upload

---
+++ sample-dm-function.js
@@ -0,0 +1,40 @@
+const TARGET_ENDPOINT = "https://api.slack.com/methods/chat.postMessage"; // This endpoint will change depending on how Charon sets up the app
+
+// Below code will need to be updated. Included here to show intended structure of the function.  
+export function handleEntityNotification (data: any) {
+
+    const type = data.meta.eventType; // This is from the webhook
+    const entityType = data.primaryProfile.meta.entityType; 
+    if (type === "ENTITY_UPDATED" && (entityType === "ce_referral")) {
+        var candidateName = data.candidateFirstName + data.candidateLastName; // First and Last name of candidate. Will pass in Slakc messages to referrers. 
+        var referrerEmail = data.meta;// Add streams value for referrer email -- add logic to grab the one that ends in @yext.com
+        var referrerFirstName = data.c_yexterWhoReferredThisPerson.firstName; // Pull first name of referrer to use when addressing in Slack message. 
+        var newStatus = data.changedFields.fieldNames.c_referralStatus; // This path should be the field value of c_referralStatus (In Review, Offer, Hired, Rejected). This will be used to determine the body text to send in the message below. 
+        return updateMessageHandler(candidateName, referrerEmail, referrerFirstName, newStatus);
+    }
+    return null;
+}
+
+// Make a call to https://api.slack.com/methods/users.lookupByEmail to find the “user_id” in Slack that matches the email address from Yext (referrerEmail)
+
+export function updateMessageHandler(entityId: string, actorLabel: string, updatedFields:string) {
+    var message = "Hi " + referrerFirstName + ", " +  "\nYour referral for " + candidateName + "has been updated." + "\nReferral stage has been changed to: " + newStatus; //Edit this with language for each referral status change - different language for each
+    return postRequest(message);
+}
+
+// Update below code to Post a message to the user who referred the candidate using chat.postMessage (Endpoint: https://api.slack.com/methods/chat.postMessage) 
+// Pass Slack user_id pulled in call above to determine who to send the message to. Message body will include candidateName, referrerFirstName, and newStatus vars above). Baked into message text in appendix depending on which the status the candidate has been updated to.
+export function postRequest(message: string){
+    console.log("here")
+    console.log(message)
+    var payload = {text:message};
+    //payload[field] = message;
+    const request = new Request(TARGET_ENDPOINT, {
+    method: 'POST',
+    body: JSON.stringify(payload),
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+    return fetch(request);
+}
